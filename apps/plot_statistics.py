3ee4979a730ba419f3c076f577951f44e38f1814
Rodrigo Nazar <rnazar@uc.cl>
2020-02-24 15:54:00 -0300

    a

--- apps/plot_statistics.py
+++ apps/plot_statistics.py
@@ -1,51 +1,50 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from pymongo import MongoClient
 
 
-def main(db_name: str, dev_eui: str) -> None:
+def main(ip_addr: str, db_name: str, dev_eui: str) -> None:
     """
     Plots the histogram and the dataframe of the data.csv
     :param csv_file: File produced by get_csv.py
     :return: None
     """
 
-    client = MongoClient()
+    client = MongoClient(ip_addr, 27017)
     db = client[db_name]
-    # client_sample_rate = '30T'
+
     table = db['tilt_t_s']
-    # pipeline = [{'$project': {'data': 1, '_id': 0}}, {'$unwind': '$data'}]
+    pipeline = [{'$match': {'devEUI': dev_eui}},
+                {'$project': {'data': 1, '_id': 0}}, {'$unwind': '$data'},
+                {'$project': {'ts': '$data.ts'}}]
 
+    # table = table.find({"devEUI": dev_eui}, projection={"data": 1, "_id": 0})
+    # print(table.aggregate(pipeline))
 
-    # df = pd.DataFrame(table.aggregate(pipeline))
-    df = pd.DataFrame(table)
-    # df.set_index('ts', inplace=True)
+    df = pd.DataFrame(table.aggregate(pipeline))
+    df = df.diff().dropna()
+    # df['ts'] = df['ts'].apply(lambda x: x.seconds())
 
-    print(df)
-    # df.index = pd.to_datetime(df.index)
-    # df = df.resample(client_sample_rate).mean()
-    # df = df.diff(1).ffill().dropna()
-    # df.to_csv(csv_file)
-    #
-    # df = pd.read_csv(csv_file, parse_dates=['ts'], index_col='ts')
-    # print(df.describe())
-    # print("numpy", df.values)
-    #
-    # gauss_mean, gauss_std = df['T'].mean(), df['T'].std()
-    # print(f"gaussian params for data mean:{gauss_mean}, std: {gauss_std}"
-    #
-    # df.plot(kind='hist', bins=30, density=True)
-    # df.plot()
+    df['ts'] = df['ts'].apply(lambda x: x / np.timedelta64(1, 'm'))
+    df.plot.hist(bins=100)
+    plt.show()
+    # print(df['ts'].dt.seconds())
+    # df.ts = pd.to_datetime(df.ts)
+    # df.ts = df.ts.dt.total_seconds()
+    # df.diff(-1)['ts'].dropna().plot(kind='hist')
     # plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--db_name', type=str, required=True,
+    parser.add_argument('--ip_addr', type=str, required=True,
+                        help="IP adress of the database to query")
+    parser.add_argument('--db_name', type=str, default='iot-ripios',
                         help="Mongo db with tilt data")
     parser.add_argument('--dev_eui', type=str, required=True,
                         help="The device EUI to query")
     cmd_args = parser.parse_args()
-    main(cmd_args.csv_file)
+    main(cmd_args.ip_addr, cmd_args.db_name, cmd_args.dev_eui)
